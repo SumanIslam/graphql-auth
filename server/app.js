18bd7522697c2c0c5eebf489f60320ac50ba6743
Suman Islam <sumanislam1416@gmail.com>
2022-07-05 11:57:37 +0600

    create a simple server using express

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const session = require('mongoose');
+const passport = require("passport");
+
+const app = express();
+
+
+module.exports = app;
