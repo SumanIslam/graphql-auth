767a85d0cb5b1fd0675974e888f0ba2c5e175210
Suman Islam <sumanislam1416@gmail.com>
2022-07-05 12:16:53 +0600

    connect mongodb atlas with our application

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
 const session = require('mongoose');
 const passport = require("passport");
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const app = require('./app');
+const { mongoConnect } = require("./services/mongo");
 
-app.listen(4000, () => {
-  console.log('Server is listening on port 4000...');
-});
+async function startServer() {
+  await mongoConnect();
+
+  app.listen(4000, () => {
+		console.log("Server is listening on port 4000...");
+	});
+}
+
+startServer();
