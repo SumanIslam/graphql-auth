6ebac20fcc91f32b2637254e5b6b0efbc5d0e68f
Suman Islam <sumanislam1416@gmail.com>
2022-07-05 12:27:18 +0600

    add graphql route and pass schema to it

--- server/app.js
+++ server/app.js
@@ -3,7 +3,13 @@ const expressGraphQL = require('express-graphql');
 const session = require('mongoose');
 const passport = require("passport");
 
+const schema = require('./schema/schema');
+
 const app = express();
 
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
 
 module.exports = app;
